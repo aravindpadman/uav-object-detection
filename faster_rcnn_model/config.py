f644a96b5666e180913d000c694b9fac2850b65b
aravindpadman <aravindpadman@gmail.com>
2021-11-29 15:33:56 +0530

    modified config

--- faster_rcnn_model/config.py
+++ faster_rcnn_model/config.py
@@ -1,8 +1,10 @@
-"""config file"""
+"""
+config file
+"""
 
 class Config:
-    OUTPUT_DIR = "/home/welcome/uav-object-detection/outputs/exp-2"
-    DATA_DIR = '/home/welcome/uav-object-detection/data'
+    OUTPUT_DIR = "/data/uav-object-detection/outputs/exp-1"
+    DATA_DIR = "/data/uav-object-detection/data/"
     NUM_CLASSES = 12
     SAVE_CHECKPOINT = False
 
@@ -12,7 +14,6 @@ class Config:
     class TEST:
         BATCH_SIZE = 1
         CHECKPOINT_FILE_PATH = ""
-        #CHECKPOINT_FILE_PATH = "/home/welcome/uav-object-detection/outputs/exp-1/checkpoints/checkpoint_epoch_00003.pyth"
 
     class TENSORBOARD:
         VISUALIZE = False
